dcb0adf1d072522be5992fb4ac0ccbf8c4f6e317
Chidozie <chidough14@gmail.com>
2025-05-02 09:50:55 +0200

    add profile pic to post

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,10 +1,12 @@
 import { useMutation } from '@tanstack/react-query'
 import React, { ChangeEvent, useState } from 'react'
 import { supabase } from '../supabase-client'
+import { useAuth } from '../context/AuthContext'
 
 interface PostInput {
   title: string
   content: string
+  avatar_url: string | null
 }
 
 const createPost = async (post: PostInput, imageFile: File) => {
@@ -29,6 +31,8 @@ const CreatePost = () => {
   const [content, setContent] = useState<string>("")
   const [selectedFile, setSelectedFile] = useState<File | null>(null)
 
+  const { user } = useAuth()
+
   const { mutate, isError, isPending } = useMutation({
     mutationFn: (data: { post: PostInput, imageFile: File }) => {
       return createPost(data.post, data.imageFile)
@@ -40,7 +44,7 @@ const CreatePost = () => {
 
     if (!selectedFile) return
 
-    mutate({ post: { title, content }, imageFile: selectedFile })
+    mutate({ post: { title, content, avatar_url: user?.user_metadata.avatar_url || null }, imageFile: selectedFile })
   }
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
