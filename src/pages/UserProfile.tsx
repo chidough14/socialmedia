1ba339b441aadff7a2c055290a5e06e782eb8e6f
Chidozie <chidough14@gmail.com>
2025-05-27 15:00:31 +0200

    bug fix

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -14,7 +14,6 @@ const fetchUserByEmail = async (email: string): Promise<User> => {
 
 
 const UserProfile = () => {
-  const { user } = useAuth()
 
   const { email } = useParams<{email: string}>()
   
